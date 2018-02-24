a92a8167fac2b1fbbd578e203273ca8b32085c91
Stewart Coleman <stewartcoleman@charless-macbook-air.local>
2018-02-23 20:19:45 -0800

    Initial Commit

---
+++ Swift Fun 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun 1
+//
+//  Created by Stewart Coleman on 2/23/18.
+//  Copyright © 2018 Stewart Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
